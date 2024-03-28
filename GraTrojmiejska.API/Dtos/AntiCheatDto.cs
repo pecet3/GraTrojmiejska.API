0a97aedef406cd9558b0a8f8300986bb3ab79a76
pecet <pecet3107@gmail.com>
2024-03-28 21:08:46 +0100

    new dtos

---
+++ GraTrojmiejska.API/Dtos/AntiCheatDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GraTrojmiejska.API.Dtos
+{
+    public record class AntiCheatDto
+        (
+        string MapPointId,
+        string UserId,
+        CoordinateDto UserCurrentCoordinate
+        );
+}
---
+++ GraTrojmiejska.API/Dtos/AttemptCaptureDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GraTrojmiejska.API.Dtos
+{
+    public record class AttemptCaptureDto
+        (
+        string MapPointId,
+        string UserId,
+        CoordinateDto UserCurrentCoordinate
+        );
+}
